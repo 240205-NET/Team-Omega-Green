2196a4e79f57d922346a1fdb04de0d0687de1e7d
ssams01 <airjordanfranchize5@gmail.com>
2024-03-12 14:33:56 -0400

    Have navbar working and home and default route added and working

--- Lit.Client/Lit.Client.App/src/app/app.component.ts
+++ Lit.Client/Lit.Client.App/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './boostrap.css']
 })
 export class AppComponent {
   title = 'Lit.Client.App';
