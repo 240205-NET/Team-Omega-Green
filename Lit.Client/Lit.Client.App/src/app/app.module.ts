0d3e5e89b24fbbeb19018fcb2c3be9ad68a890d6
meryemselicioglu <98435791+meryemselici@users.noreply.github.com>
2024-03-13 15:02:08 -0500

    changes

--- Lit.Client/Lit.Client.App/src/app/app.module.ts
+++ Lit.Client/Lit.Client.App/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BookDetailComponent } from './book-detail/book-detail.component'
 
 
+const appRoutes: Routes = [
+  {path: 'book-detail/:id', component: BookDetailComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
