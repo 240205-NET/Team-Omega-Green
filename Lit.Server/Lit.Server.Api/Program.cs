9ab5b7f55b2a2f1442cdc290d2e06127974f5b02
Matthew Smilansky <msmilansky@gmail.com>
2024-03-16 16:26:05 -0700

    feat: Add CORS

--- Lit.Server/Lit.Server.Api/Program.cs
+++ Lit.Server/Lit.Server.Api/Program.cs
@@ -10,7 +10,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
