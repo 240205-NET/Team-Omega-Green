0de1882878833f3d9e8615615b299216fd1a2202
Lawrence "Vayro" Biteranta <lbluck@gmail.com>
2024-03-16 22:08:00 -0700

    Feat: Functional SearchBox
    added proxy. Ask Lawrence for more information on how to access the LitAPI with the proxy if your methods aren't returning data

--- Lit.Client/Lit.Client.App/src/app/search-results/search-results.component.ts
+++ Lit.Client/Lit.Client.App/src/app/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-results',
@@ -8,13 +9,68 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchResultsComponent implements OnInit {
   searchQuery: string = '';
+  books: Book[] = [];
+  filteredBooks: Book[] = []; // Array to hold filtered books!
+  apiUrl: string = 'api/books';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    // Retrieve the search query from the route parameters
     this.route.params.subscribe(params => {
       this.searchQuery = params['query'];
+      this.fetchBooks(); // Call the method to fetch books!!
     });
   }
+
+  fetchBooks(): void {
+    this.httpClient.get<Book[]>(this.apiUrl).subscribe(
+      (books: Book[]) => {
+        this.books = books;
+        this.filterBooks(); // Filter books after fetching!!!
+        console.log(this.filteredBooks);
+      },
+      error => {
+        console.error('Error fetching books:', error);
+        this.searchQuery = `Error searching for: ${this.searchQuery}. Try again.`;
+      }
+    );
+  }
+
+  filterBooks(): void {
+    if (this.searchQuery.trim() === '') {
+      // If search query is empty, display all books.. actually this won't ever be called because I blocked empty search queries in the searchbox component but just in case...
+      this.filteredBooks = this.books;
+    } else {
+      // Filter books based on search query
+      this.filteredBooks = this.books.filter(book =>
+        this.bookContainsQuery(book)
+      );
+    }
+  }
+
+  bookContainsQuery(book: Book): boolean {
+    const query = this.searchQuery.toLowerCase();
+    return (
+      book.title.toLowerCase().includes(query) ||
+      book.isbn.toLowerCase().includes(query) ||
+      book.author.toLowerCase().includes(query)
+    );
+  }
+  
+  goToBookDetail(bookID:string){
+
+    console.log(bookID);
+  }
+}
+
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  isbn: string;
+  categoryId: number;
 }
