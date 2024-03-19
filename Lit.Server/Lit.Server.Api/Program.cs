8515c30c663f7d4e09f2403859d3ad4f93067964
Matthew Smilansky <msmilansky@gmail.com>
2024-03-18 23:11:49 -0700

    fix: remove CORS? smh

--- Lit.Server/Lit.Server.Api/Program.cs
+++ Lit.Server/Lit.Server.Api/Program.cs
@@ -12,12 +12,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddCors();
+// builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+// app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
